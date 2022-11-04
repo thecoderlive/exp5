6f0501c2fdc3c8a85dd70af5b4535cba9a98c5ea
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-05 00:01:41 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "1bdc2f42-21ec-49c4-8c6b-05f7ee91047f",
+    "home_feed": [
+        {
+            "id": "a8bbd27f-c915-4010-bddc-75b437c4b448",
+            "artist_name": "myself",
+            "home_carousel": [
+                {
+                    "id": "f9806db0-ad4a-41f0-af60-6530e90842a2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/255.png"
+                },
+                {
+                    "id": "baee519b-269e-41fa-9242-d22206877311",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/56.png"
+                },
+                {
+                    "id": "068fe55b-e52d-4eca-b27f-05735acc0d1a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/133.png"
+                },
+                {
+                    "id": "60442341-db90-45fb-a798-6039462935f2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/62.png"
+                },
+                {
+                    "id": "1f90cb00-651e-4145-af09-33c7cbde8a30",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/249.png"
+                }
+            ],
+            "art_caption": "fight",
+            "art_description": "Democratic physical community soldier. Step require only thousand. Ever sure yourself present."
+        },
+        {
+            "id": "7b8e1022-9ef9-46e1-beff-133bc5e3ff5c",
+            "artist_name": "where",
+            "home_carousel": [
+                {
+                    "id": "9f21ba01-06ce-4388-8f7a-5998b3b5628a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/229.png"
+                },
+                {
+                    "id": "19a21917-1761-4da8-bd00-d0fbef49e3d4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/8.png"
+                },
+                {
+                    "id": "4fa978fc-6454-4013-87b0-338147a896f2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/2.png"
+                },
+                {
+                    "id": "0844b50b-5472-4ee9-bf87-9cf057f785db",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/95.png"
+                },
+                {
+                    "id": "ce042d6e-c9c4-4ec6-a2b4-834c0ebcdb11",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/11.png"
+                }
+            ],
+            "art_caption": "charge",
+            "art_description": "Sister fast blue table quickly. Experience room write consumer record accept. Top challenge evening oil ahead here."
+        },
+        {
+            "id": "a991bacd-1109-49ec-90e9-85ec4d734f5c",
+            "artist_name": "relate",
+            "home_carousel": [
+                {
+                    "id": "13830576-5ec7-48b7-ae93-3da87686bb4a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/117.png"
+                },
+                {
+                    "id": "b2f1f53e-02e7-44dc-8700-a1cf1ae7402b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/149.png"
+                },
+                {
+                    "id": "720de0c0-322a-4545-a2e1-89ba87d41681",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/64.png"
+                },
+                {
+                    "id": "7127eeb4-560a-41de-ab4d-72001ea9aab1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/5.png"
+                },
+                {
+                    "id": "b7487f96-8ee7-432e-835a-23acd2fdb19e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/9.png"
+                }
+            ],
+            "art_caption": "single",
+            "art_description": "End break recent fight thus. Various push company ball feeling. Experience skill manage but."
+        },
+        {
+            "id": "b40cb29e-ff48-4668-b5b5-11942a5b7dee",
+            "artist_name": "record",
+            "home_carousel": [
+                {
+                    "id": "7214aa1c-a7d8-42e1-83e5-426111962d2c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/237.png"
+                },
+                {
+                    "id": "ecad893d-e82f-49b3-bdc0-311d9a9d7941",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/293.png"
+                },
+                {
+                    "id": "47c12269-4a6f-4e93-9f53-759936e47bfa",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/245.png"
+                },
+                {
+                    "id": "d491d6c3-a5fb-4ce2-83ce-b7864200e1e9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/158.png"
+                },
+                {
+                    "id": "59f1b716-c553-4d77-8247-25486d8be700",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/115.png"
+                }
+            ],
+            "art_caption": "position",
+            "art_description": "Kitchen put grow my move. Rule development fire fine likely factor again. Data year human population leader her."
+        },
+        {
+            "id": "22b0d81b-01ff-46dd-809d-9c54901dfc6b",
+            "artist_name": "radio",
+            "home_carousel": [
+                {
+                    "id": "1444941e-ce8d-4fd2-9288-c31ffb267849",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/212.png"
+                },
+                {
+                    "id": "5aa61964-a598-4fc5-8f0f-a99437ccb301",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/107.png"
+                },
+                {
+                    "id": "d9a48cb9-236b-4933-8b8c-f7a81988a451",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/181.png"
+                },
+                {
+                    "id": "4caf695e-e6b4-4e9e-a41c-bc56549a6faf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/127.png"
+                },
+                {
+                    "id": "c598fa76-a404-4f42-95a6-cf98f8c26d4a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/225.png"
+                }
+            ],
+            "art_caption": "north",
+            "art_description": "Stay put play anything blue dinner six perhaps. Attorney become claim. Person despite rather black owner station."
+        }
+    ]
+}
